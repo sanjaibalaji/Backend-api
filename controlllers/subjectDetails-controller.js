c7b067118bd4514204a21aa29fc0f726c0333d8a
sanjaibalaji <sanjainsivam@gmail.com>
2024-01-10 16:03:45 +0530

    sub_code

--- controlllers/subjectDetails-controller.js
+++ controlllers/subjectDetails-controller.js
@@ -2,15 +2,15 @@ const db = require("../models");
 const SubjectDetails = db.subject_details;
 
 exports.subjects = async (req, res, next) => {
-    const {sub_id,sub_name} = req.body;
-    if (!sub_id) {
+    const {sub_code,sub_name} = req.body;
+    if (!sub_code) {
         return res.status(400).json({ error: 'Enter the subject id' });
       }
       if(!sub_name) {
         return res.status(400).json({ error: 'Enter the subject name' });
       }
       const subjects = {
-        sub_id: req.body.sub_id,
+        sub_code: req.body.sub_code,
         sub_name:req.body.sub_name,
       }
       const result = await SubjectDetails.create(subjects)
